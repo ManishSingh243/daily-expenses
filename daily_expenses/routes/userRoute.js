e9368db5aeb22306410555953ddf2b408db890d3
ManishSingh243 <87477768+ManishSingh243@users.noreply.github.com>
2023-10-12 22:41:32 +0530

    Update userRoute.js

--- daily_expenses/routes/userRoute.js
+++ daily_expenses/routes/userRoute.js
@@ -9,4 +9,7 @@ router.post("/add-expense", authenticate, expenseController.postExpense)
 //userExpenses
 router.get("/user-expenses", authenticate, expenseController.getExpense)
 
+//delete-expense
+router.delete("/delete-expense/:expenseId", expenseController.deleteExpense);
+
 module.exports = router;
