8a0d887336d032fcc043e13414ef8b30705f45fd
ManishSingh243 <87477768+ManishSingh243@users.noreply.github.com>
2023-11-07 09:32:01 +0530

    Create amountController.js

---
+++ daily_expenses/controllers/amountController.js
@@ -0,0 +1,29 @@
+const db = require('../util/database')
+
+exports.postAmount = async (req, res) => {
+    try{
+        console.log("program initiated")
+        const [users] = await db.query("SELECT userid, name FROM users");
+        console.log("line 5 executed successfully")
+        const arr = [];
+        console.log("arr created successfully")
+
+        for (const user of users) {
+            console.log("entered into an array successfully", user.userid)
+            const [totalAmountResult] = await db.query("SELECT SUM(amount) as totalAmount FROM userexpense WHERE userid = ?", [user.userid]);
+            console.log("query executed successfully");
+            const totalAmount = totalAmountResult[0].totalAmount || 0;
+            console.log("amount calculated successfully")
+
+            arr.push({
+                userName: user.name,
+                Amount: totalAmount
+            });
+            console.log("data pushed successfully")
+        }
+        console.log(arr)
+        res.send(arr);
+    } catch(err){
+        res.status(500).json({message: "Internal error"})
+    }
+}
