c9f0f7529c4baa742b5fc8acf2ff434f8cbb3da8
ManishSingh243 <87477768+ManishSingh243@users.noreply.github.com>
2023-12-09 01:50:20 +0530

    Update passwordRoute.js

--- daily_expenses/routes/passwordRoute.js
+++ daily_expenses/routes/passwordRoute.js
@@ -4,5 +4,6 @@ const passwordController = require('../controllers/passwordController')
 const router = express.Router();
 
 router.post('/forgot-password', passwordController.postPassword)
+router.post('/reset-password/:requestId', passwordController.postResetPassword)
 
 module.exports = router;
