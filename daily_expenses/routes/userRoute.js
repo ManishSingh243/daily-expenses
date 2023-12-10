0849c9bfbae4bc6d95b9fd105ff48de1f18641d2
ManishSingh243 <87477768+ManishSingh243@users.noreply.github.com>
2023-12-10 10:38:19 +0530

    Update userRoute.js

--- daily_expenses/routes/userRoute.js
+++ daily_expenses/routes/userRoute.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const expenseController = require('../controllers/expenseController')
-const authenticate = require('../middleware/authMiddleware')
+const expenseController = require('../controllers/expenseController');
+const authenticate = require('../middleware/authMiddleware');
 
-//addExpense
-router.post("/add-expense", authenticate, expenseController.postExpense)
+router.post('/add-expense', authenticate, expenseController.postExpense);
+router.get('/user-expenses', authenticate, expenseController.getExpense);
+router.delete('/delete-expense/:expenseId', expenseController.deleteExpense);
+router.post('/razorpay', authenticate, expenseController.postRazorpay);
+router.post('/capture-payment', authenticate, expenseController.postPayment);
 
-//userExpenses
-router.get("/user-expenses", authenticate, expenseController.getExpense)
-
-//delete-expense
-router.delete("/delete-expense/:expenseId", expenseController.deleteExpense);
-
-router.post("/razorpay", authenticate, expenseController.postRazorpay);
-
-router.post("/capture-payment", authenticate, expenseController.postPayment)
+// New endpoint for downloading expense file
+router.get('/download-expense-file', authenticate, expenseController.getExpenseFile);
 
 module.exports = router;
