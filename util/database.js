9dd94b880e7a2179e9a2c56da876eaf7d28b11aa
ManishSingh243 <87477768+ManishSingh243@users.noreply.github.com>
2023-12-21 01:07:41 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,9 +1,23 @@
-const mysql2 = require('mysql2/promise');
+/*const mysql2 = require('mysql2/promise');
 const db = mysql2.createPool({
     host: 'localhost',
     user: 'root',
     password: 'password',
     database: 'sys'
 })
+
+module.exports = db;*/
+
+const mysql2 = require('mysql2/promise');
+
+const db = mysql2.createPool({
+    host: 'database-2.c10ey06e6xuk.ap-south-1.rds.amazonaws.com',
+    user: 'john',
+    password: 'mufilrahman',
+    database: 'sys',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
 module.exports = db;
