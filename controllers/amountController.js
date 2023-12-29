2d1de0a6e7dd85a2f80675e345385b0f81748a97
ManishSingh243 <87477768+ManishSingh243@users.noreply.github.com>
2023-12-29 14:36:26 +0530

    Update amountController.js

--- controllers/amountController.js
+++ controllers/amountController.js
@@ -1,8 +1,9 @@
 const db = require('../util/database')
 
 exports.postAmount = async (req, res) => {
+    const userId = req.user.userId;
     try{
-        const [users] = await db.query("SELECT userid, name, totalexpense FROM users");
+        const [users] = await db.query("SELECT name, email, totalexpense FROM users WHERE userid = ?", [userId]);
         res.send(users);
     } catch(err){
         res.status(500).json({message: "Internal error"})
