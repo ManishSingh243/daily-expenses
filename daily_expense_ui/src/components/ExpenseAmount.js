43dde420b8c643c7aa933efc78e181f08c9fa9db
ManishSingh243 <87477768+ManishSingh243@users.noreply.github.com>
2023-12-29 14:38:35 +0530

    Update ExpenseAmount.js

--- daily_expense_ui/src/components/ExpenseAmount.js
+++ daily_expense_ui/src/components/ExpenseAmount.js
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Table } from "react-bootstrap";
 
 export default function ExpenseAmount() {
   const [amount, setAmount] = useState([]);
+  const token = localStorage.getItem("token");
+  const config = {
+    headers: {
+      "x-auth-token": token,
+    },
+  };
 
   const fetchAmount = async () => {
     try {
-      const response = await axios.get("/expense-amount");
+      const response = await axios.get("/expense-amount", config);
       setAmount(response.data);
     } catch (err) {
       console.log(err);
@@ -19,14 +26,25 @@ export default function ExpenseAmount() {
 
   return (
     <div>
-      <h1>Amounts of respective users:</h1>
-      <ul>
-        {amount.map((item) => (
-          <li key={item.userid}>
-            {item.name}: {item.totalexpense}
-          </li>
-        ))}
-      </ul>
+      <h1>Total Expense:</h1>
+      <Table striped bordered responsive>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {amount.map((item) => (
+            <tr key={item.userid}>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.totalexpense}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 }
